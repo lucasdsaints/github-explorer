fd8df07b954e6cba53f9638f4aae4e79fe3b0a3c
Lucas Santos <lucas.assis.santos96@gmail.com>
2021-03-14 12:40:28 -0300

    busca de repositórios por usuário ou organzação

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -10,26 +10,57 @@ interface Repository {
 }
 
 export function RepositoryList() {
+  const [repoType, setRepoType] = useState('');
+  const [loginName, setLoginName] = useState('');
   const [repositories, setRepositories] = useState<Repository[]>([]);
 
   useEffect(() => {
-    fetch('https://api.github.com/orgs/rocketseat/repos')
-      .then(response => response.json())
+    if (!repoType || !loginName) {
+      setRepoType('');
+      return;
+    }
+
+    const reposUrl = `https://api.github.com/${repoType}/${loginName}/repos`;
+    fetch(reposUrl)
+      .then(response => response.ok ? response.json() : [])
       .then(data => setRepositories(data))
-  }, []);
+      .then(() => setRepoType(''));
+  }, [repoType]);
 
   return (
-    <section className="repository-list">
-      <h1>Lista de repositórios</h1>
+    <div className="repository-container">
+      <h1>Busca de repositórios GitHub</h1>
+
+      <div className="search-box">
+        <input
+          type="text"
+          name="loginName"
+          placeholder="Login"
+          onChange={(e) => setLoginName(e.target.value)}
+        />
+        <select
+          name="type"
+          value={ repoType }
+          onChange={(e) => setRepoType(e.target.value)}
+        >
+          <option value="">Selecione para Pesquisar</option>
+          <option value="users">Pesquisar Usuário</option>
+          <option value="orgs">Pesquisar Organização</option>
+        </select>
+      </div>
 
       <ul>
-        {repositories.map(repository => (
-          <RepositoryItem
-            key={ repository.name }
-            repository={ repository }
-          />
-        ))}
+        {
+          repositories.length
+          ? repositories.map(repository => (
+            <RepositoryItem
+              key={ repository.name }
+              repository={ repository }
+            />
+          ))
+          : 'Ops, não há dados para a pesquisa atual... Tente outros termos!'
+        }
       </ul>
-    </section>
+    </div>
   );
 };
